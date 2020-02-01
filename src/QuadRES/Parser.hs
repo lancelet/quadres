8b715e26974f72fb312bb9b1ddd0cf665b604a17
Jonathan Merritt <j.s.merritt@gmail.com>
2020-02-02 00:07:16 +1100

    Parsing switches

--- src/QuadRES/Parser.hs
+++ src/QuadRES/Parser.hs
@@ -22,17 +22,47 @@ type Parsec e s a = MP.ParsecT e s Identity a
 
 type Parser a = Parsec Void Text a
 
+-- | Parse whitespace and optional switches.
+ws :: Parser RES.Switches
+ws = pWhitespace *> pSwitches
+
+-- | Parse a 'RES.Switches'.
+--
+-- >>> sw = "![red] ![noshade,sep=4.3]"
+-- >>> MP.parseMaybe pSwitches sw
+-- Just (Switches [Switch [SwColor Red],Switch [SwShadeState NoShade,SwSep (Sep 4.30)]])
+pSwitches :: Parser RES.Switches
+pSwitches = RES.Switches <$> MP.many pSwitch
+
+-- | Parse a 'RES.Switch'.
+--
+-- >>> sw = "![blue, noshade, sep=1.2]"
+-- >>> MP.parseMaybe pSwitch sw
+-- Just (Switch [SwColor Blue,SwShadeState NoShade,SwSep (Sep 1.20)])
+--
+-- A plain exclamation point can also be a switch. This has no interpretation
+-- other than as a possible marker to a human reader:
+-- >>> MP.parseMaybe pSwitch "!"
+-- Just (Switch [])
+pSwitch :: Parser RES.Switch
+pSwitch =
+  RES.Switch
+    <$> (  MP.single '!'
+        *> MP.option [] (pBracketedList pSwitchArg)
+        <* pWhitespace
+        )
+
 -- | Parse a 'RES.SwitchArg'.
 --
 -- >>> MP.parseMaybe pSwitchArg "green"
 -- Just (SwColor Green)
 pSwitchArg :: Parser RES.SwitchArg
 pSwitchArg =
-    (RES.SwColor <$> pColor)
-        <|> (RES.SwShadeState <$> pShadeState)
-        <|> (RES.SwSep <$> pSep)
-        <|> (RES.SwFitState <$> pFitState)
-        <|> (RES.SwMirrorState <$> pMirrorState)
+  (RES.SwColor <$> pColor)
+    <|> (RES.SwShadeState <$> pShadeState)
+    <|> (RES.SwSep <$> pSep)
+    <|> (RES.SwFitState <$> pFitState)
+    <|> (RES.SwMirrorState <$> pMirrorState)
 
 -- | Parse a 'RES.FitState'.
 --
@@ -43,8 +73,8 @@ pSwitchArg =
 -- Just NoFit
 pFitState :: Parser RES.FitState
 pFitState =
-    ((MP.chunk "fit" $> RES.Fit) <|> (MP.chunk "nofit" $> RES.NoFit))
-        <?> "(fit|nofit)"
+  ((MP.chunk "fit" $> RES.Fit) <|> (MP.chunk "nofit" $> RES.NoFit))
+    <?> "(fit|nofit)"
 
 -- | Parse a 'RES.MirrorState'.
 --
@@ -55,9 +85,8 @@ pFitState =
 -- Just NoMirror
 pMirrorState :: Parser RES.MirrorState
 pMirrorState =
-    ((MP.chunk "mirror" $> RES.Mirror) <|> (MP.chunk "nomirror" $> RES.NoMirror)
-        )
-        <?> "(mirror|nomirror)"
+  ((MP.chunk "mirror" $> RES.Mirror) <|> (MP.chunk "nomirror" $> RES.NoMirror))
+    <?> "(mirror|nomirror)"
 
 -- | Parse a 'RES.ShadeState'.
 --
@@ -68,8 +97,8 @@ pMirrorState =
 -- Just NoShade
 pShadeState :: Parser RES.ShadeState
 pShadeState =
-    ((MP.chunk "shade" $> RES.Shade) <|> (MP.chunk "noshade" $> RES.NoShade))
-        <?> "(shade|noshade)"
+  ((MP.chunk "shade" $> RES.Shade) <|> (MP.chunk "noshade" $> RES.NoShade))
+    <?> "(shade|noshade)"
 
 -- | Parse a 'RES.Sep'.
 --
@@ -87,24 +116,24 @@ pSep = RES.Sep <$> (MP.chunk "sep=" *> pRealN) <?> "sep=<real>"
 -- Just Blue
 pColor :: Parser RES.Color
 pColor =
-    (   (MP.chunk "black" $> RES.Black)
-        <|> (MP.chunk "red" $> RES.Red)
-        <|> (MP.chunk "green" $> RES.Green)
-        <|> (MP.chunk "blue" $> RES.Blue)
-        <|> (MP.chunk "white" $> RES.White)
-        <|> (MP.chunk "aqua" $> RES.Aqua)
-        <|> (MP.chunk "fuchsia" $> RES.Fuchsia)
-        <|> (MP.chunk "gray" $> RES.Gray)
-        <|> (MP.chunk "lime" $> RES.Lime)
-        <|> (MP.chunk "maroon" $> RES.Maroon)
-        <|> (MP.chunk "navy" $> RES.Navy)
-        <|> (MP.chunk "olive" $> RES.Olive)
-        <|> (MP.chunk "purple" $> RES.Purple)
-        <|> (MP.chunk "silver" $> RES.Silver)
-        <|> (MP.chunk "teal" $> RES.Teal)
-        <|> (MP.chunk "yellow" $> RES.Yellow)
-        )
-        <?> "Color"
+  (   (MP.chunk "black" $> RES.Black)
+    <|> (MP.chunk "red" $> RES.Red)
+    <|> (MP.chunk "green" $> RES.Green)
+    <|> (MP.chunk "blue" $> RES.Blue)
+    <|> (MP.chunk "white" $> RES.White)
+    <|> (MP.chunk "aqua" $> RES.Aqua)
+    <|> (MP.chunk "fuchsia" $> RES.Fuchsia)
+    <|> (MP.chunk "gray" $> RES.Gray)
+    <|> (MP.chunk "lime" $> RES.Lime)
+    <|> (MP.chunk "maroon" $> RES.Maroon)
+    <|> (MP.chunk "navy" $> RES.Navy)
+    <|> (MP.chunk "olive" $> RES.Olive)
+    <|> (MP.chunk "purple" $> RES.Purple)
+    <|> (MP.chunk "silver" $> RES.Silver)
+    <|> (MP.chunk "teal" $> RES.Teal)
+    <|> (MP.chunk "yellow" $> RES.Yellow)
+    )
+    <?> "Color"
 
 -- | Parse a 'RES.ShadePattern'.
 --
@@ -119,12 +148,12 @@ pShadePattern = (RES.ShadePattern <$> NonEmpty.some pSide) <?> "Shade pattern"
 -- Just SideS
 pSide :: Parser RES.Side
 pSide =
-    (   (MP.single 't' $> RES.SideT)
-        <|> (MP.single 'b' $> RES.SideB)
-        <|> (MP.single 's' $> RES.SideS)
-        <|> (MP.single 'e' $> RES.SideE)
-        )
-        <?> "Side (t,b,s,e)"
+  (   (MP.single 't' $> RES.SideT)
+    <|> (MP.single 'b' $> RES.SideB)
+    <|> (MP.single 's' $> RES.SideS)
+    <|> (MP.single 'e' $> RES.SideE)
+    )
+    <?> "Side (t,b,s,e)"
 
 -- | Parse a square-bracket-terminated, comma-separated list of items.
 --
@@ -147,22 +176,22 @@ pSide =
 -- Nothing
 pBracketedList :: Parser a -> Parser [a]
 pBracketedList pItem = do
-    MP.single '[' $> ()
-    pWhitespace
-    items <- MP.optional (pList pItem)
-    MP.single ']' $> ()
-    pure (fromMaybe [] items)
+  MP.single '[' $> ()
+  pWhitespace
+  items <- MP.optional (pList pItem)
+  MP.single ']' $> ()
+  pure (fromMaybe [] items)
 
 -- | Parse a comma-separated, non-empty list of items.
 pList :: Parser a -> Parser [a]
 pList pItem = do
-    pWhitespace
-    item <- pItem
-    pWhitespace
-    optComma <- MP.optional (MP.single ',')
-    case optComma of
-        Nothing -> pure [item]
-        Just _  -> (item :) <$> pList pItem
+  pWhitespace
+  item <- pItem
+  pWhitespace
+  optComma <- MP.optional (MP.single ',')
+  case optComma of
+    Nothing -> pure [item]
+    Just _  -> (item :) <$> pList pItem
 
 -- | Gobble zero or more whitespace characters.
 pWhitespace :: Parser ()
@@ -201,20 +230,20 @@ pWhitespace = MP.takeWhileP (Just "whitespace") isWhitespace $> ()
 -- Nothing
 pRealN :: Parser RES.RealN
 pRealN = do
-    digit1opt <- MP.optional pDigit
-    digit2opt <- MP.optional (MP.single '.' *> pDigit)
-    -- by this point, we can have obtained "x", "x.x" or ".x"; but if both
-    -- digit1opt and digit2opt are empty then that's an error case
-    case (digit1opt, digit2opt) of
-      -- here we parsed nothing so far; so signal an error
-        (Nothing, Nothing) -> MP.failure Nothing Set.empty
-        -- here we parsed just the first digit, but no period and following digit
-        (Just d1, Nothing) -> pure (RES.mkRealN d1 0 0)
-        -- here we passed a period and a following digit so try for a third digit
-        (_      , Just d2) -> do
-            let d1 = fromMaybe 0 digit1opt
-            d3 <- MP.option 0 pDigit
-            pure (RES.mkRealN d1 d2 d3)
+  digit1opt <- MP.optional pDigit
+  digit2opt <- MP.optional (MP.single '.' *> pDigit)
+  -- by this point, we can have obtained "x", "x.x" or ".x"; but if both
+  -- digit1opt and digit2opt are empty then that's an error case
+  case (digit1opt, digit2opt) of
+    -- here we parsed nothing so far; so signal an error
+    (Nothing, Nothing) -> MP.failure Nothing Set.empty
+    -- here we parsed just the first digit, but no period and following digit
+    (Just d1, Nothing) -> pure (RES.mkRealN d1 0 0)
+    -- here we passed a period and a following digit so try for a third digit
+    (_      , Just d2) -> do
+      let d1 = fromMaybe 0 digit1opt
+      d3 <- MP.option 0 pDigit
+      pure (RES.mkRealN d1 d2 d3)
 
 -- | Parses a single digit into a 'Word8'.
 --
@@ -237,9 +266,9 @@ pDigit = (\c -> fromIntegral (ord c - 48)) <$> MP.satisfy isDigit <?> "Digit"
 -- True
 isNonZeroDigit :: Char -> Bool
 isNonZeroDigit c = c' >= 49 && c' <= 57
-  where
-    c' :: Int
-    c' = ord c
+ where
+  c' :: Int
+  c' = ord c
 
 -- | True if a character is a digit '0' - '9'.
 --
@@ -250,14 +279,14 @@ isNonZeroDigit c = c' >= 49 && c' <= 57
 -- False
 isDigit :: Char -> Bool
 isDigit c = c' >= 48 && c' <= 57
-  where
-    c' :: Int
-    c' = ord c
+ where
+  c' :: Int
+  c' = ord c
 
 -- | True if a character is whitespace.
 isWhitespace :: Char -> Bool
 isWhitespace c =
-    (c == ' ') || (c == '\t') || (c == '\n') || (c == '\r') || (c == '\f')
+  (c == ' ') || (c == '\t') || (c == '\n') || (c == '\r') || (c == '\f')
 
 -- $setup
 -- >>> :set -XOverloadedStrings
--- test/QuadRES/ParserTest.hs
+++ test/QuadRES/ParserTest.hs
@@ -4,8 +4,8 @@ Description : Tests for the QuadRES.Parser module.
 -}
 {-# LANGUAGE OverloadedStrings #-}
 module QuadRES.ParserTest
-    ( tests
-    )
+  ( tests
+  )
 where
 
 import qualified Hedgehog                      as H
@@ -22,40 +22,40 @@ import qualified QuadRES.RES                   as RES
 
 tests :: H.Group
 tests = H.Group
-    "QuadRES.Parser"
-    [ ("pRealN parses shown RealN values"      , prop_pRealN_trip)
-    , ("'1'-'9' are definitely non-zero digits", unit_IsNonZeroDigit_1_9)
-    , ( "isNonZeroDigit matches Data.Char.isDigit with zero exclusion"
-      , prop_IsNonZeroDigit
-      )
-    , ("'0'-'9' are definitely digits"    , unit_IsDigit_0_9)
-    , ("isDigit matches Data.Char.isDigit", prop_IsDigit)
-    ]
+  "QuadRES.Parser"
+  [ ("pRealN parses shown RealN values"      , prop_pRealN_trip)
+  , ("'1'-'9' are definitely non-zero digits", unit_IsNonZeroDigit_1_9)
+  , ( "isNonZeroDigit matches Data.Char.isDigit with zero exclusion"
+    , prop_IsNonZeroDigit
+    )
+  , ("'0'-'9' are definitely digits"    , unit_IsDigit_0_9)
+  , ("isDigit matches Data.Char.isDigit", prop_IsDigit)
+  ]
 
 prop_pRealN_trip :: H.Property
 prop_pRealN_trip = H.property $ do
-    let genDigit = Gen.word8 (Range.linear 0 9)
-    d1 <- H.forAll genDigit
-    d2 <- H.forAll genDigit
-    d3 <- H.forAll genDigit
-    let realN = RES.mkRealN d1 d2 d3
-    H.tripping realN (Text.pack . show) (MP.parseMaybe Parser.pRealN)
+  let genDigit = Gen.word8 (Range.linear 0 9)
+  d1 <- H.forAll genDigit
+  d2 <- H.forAll genDigit
+  d3 <- H.forAll genDigit
+  let realN = RES.mkRealN d1 d2 d3
+  H.tripping realN (Text.pack . show) (MP.parseMaybe Parser.pRealN)
 
 unit_IsNonZeroDigit_1_9 :: H.Property
-unit_IsNonZeroDigit_1_9 = H.withTests 1 $ H.property $ H.assert
-    (all Parser.isNonZeroDigit ['1' .. '9'])
+unit_IsNonZeroDigit_1_9 =
+  H.withTests 1 $ H.property $ H.assert (all Parser.isNonZeroDigit ['1' .. '9'])
 
 prop_IsNonZeroDigit :: H.Property
 prop_IsNonZeroDigit = H.property $ do
-    c <- H.forAll Gen.alphaNum
-    (Data.Char.isDigit c && c /= '0') === Parser.isNonZeroDigit c
+  c <- H.forAll Gen.alphaNum
+  (Data.Char.isDigit c && c /= '0') === Parser.isNonZeroDigit c
 
 unit_IsDigit_0_9 :: H.Property
 unit_IsDigit_0_9 =
-    H.withTests 1 $ H.property $ H.assert (all Parser.isDigit ['0' .. '9'])
+  H.withTests 1 $ H.property $ H.assert (all Parser.isDigit ['0' .. '9'])
 
 prop_IsDigit :: H.Property
 prop_IsDigit = H.property $ do
-    c <- H.forAll Gen.alphaNum
-    Data.Char.isDigit c === Parser.isDigit c
+  c <- H.forAll Gen.alphaNum
+  Data.Char.isDigit c === Parser.isDigit c
 
